e1eac79817827d2bc0ff06f8ae5b4fe43793bc95
srikanth-sripati <srika@Srikanth>
2018-11-26 22:46:32 +0530

    comments

---
+++ src/main/java/com/backendpoc/BackEndPocApplication.java
@@ -0,0 +1,13 @@
+package com.backendpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndPocApplication.class, args);
+		System.out.println("Hello");
+	}
+}
