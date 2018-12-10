8f4d9ca7e4324d0bf6198aee03e23bcd5d081c6b
srikanth-sripati <srikanth.sripati8195@gmail.com>
2018-12-10 09:59:30 +0530

    Cross origin

--- src/main/java/com/backendpoc/api/BackendController.java
+++ src/main/java/com/backendpoc/api/BackendController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.backendpoc.entity.StudentEntity;
 import com.backendpoc.service.StudentService;
-
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class BackendController {
 
