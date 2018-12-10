b12b77bee1f64124fb47d2be2365d4d504e5da81
srikanth-sripati <srikanth.sripati8195@gmail.com>
2018-12-10 23:29:24 +0530

    delete mapping

--- src/main/java/com/backendpoc/api/BackendController.java
+++ src/main/java/com/backendpoc/api/BackendController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -33,7 +34,7 @@ public class BackendController {
 		return service.getStudentById(id);
 	}
 	
-	@RequestMapping("/delete/{id}")
+	@DeleteMapping("/delete/{id}")
 	ResponseEntity<Integer> deleteById(@PathVariable("id") Integer id) {
 		service.deleteById(id);
 		return new ResponseEntity<Integer>(HttpStatus.OK);
